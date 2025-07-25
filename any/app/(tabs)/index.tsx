89f35b0403e03034e0ce241bf95324c6bf90a864
SikHyoKim <dogdog120@naver.com>
2025-07-25 12:00:21 +0900

    start

---
+++ any/app/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
