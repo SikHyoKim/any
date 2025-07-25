310ff3b899ec8d9e1c882a7e76ae20a56bba45c1
SikHyoKim <dogdog120@naver.com>
2025-07-25 12:55:02 +0900

    feat: 게시글 목록,상세 stack분리

--- any/app/_layout.tsx
+++ any/app/_layout.tsx
@@ -1,29 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Button } from 'react-native';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: '게시글 목록',
+          headerRight: () => (
+            // 여기에 로그인 버튼 또는 아이콘을 넣을 수 있습니다.
+            <Button title="로그인" onPress={() => {/* 로그인 로직 */}} />
+          ),
+        }}
+      />
+      <Stack.Screen name="post-list-detail" options={{ headerShown: false }} />
+    </Stack>
   );
 }
