b20b1cc5c1d94a9084fdc4d8279482d22a159177
SikHyoKim <dogdog120@naver.com>
2025-07-25 14:59:57 +0900

    유저 로그인 상태 관리 추가

---
+++ any/app/contexts/authContext.js
@@ -0,0 +1,24 @@
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '../../scripts/firebaseConfig';
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, setUser);
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, signOut: () => signOut(auth) }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
